b211baec6ab6043daf597143e7b71341c19e6ea2
Lucas Aquino <lucasbaquino@icloud.com>
2021-12-28 20:58:35 -0300

    feat: config an Ethereum wallet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 const projectId = process.env.PROJECT_ID
+const privateKey = process.env.PRIVATE_KEY
 
 module.exports = {
   networks: {
@@ -8,11 +9,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [privateKey]
     },
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [privateKey]
     }
   },
   solidity: "0.8.4",
